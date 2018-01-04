6bf5fcb04c2a7c68516ed3f6c3164b4b3b334311
Gilho Won <happy@bnbb.co.kr>
2018-01-04 15:55:38 +0630

    modify comments

--- pilots/lcd-display.js
+++ pilots/lcd-display.js
@@ -11,16 +11,16 @@
       - RS=0: Command, RS=1: Data
     * (5) RW(Read/Write) - Ground
       - R/W=0: Write, R/W=1: Read (we won't use this pin)
-    * (6) E(Enable) -  GPIO16 (Pin 40)
+    * (6) E(Enable) -  GPIO16 (Pin 32)
       - Clock. Falling edge triggered
     * (7) D0 - Bit 0 (Not used in 4-bit operation)
     * (8) D1 - Bit 1 (Not used in 4-bit operation)
     * (9) D2 - Bit 2 (Not used in 4-bit operation)
     * (10) D3 - Bit 3 (Not used in 4-bit operation)
-    * (11) D4(Bit 4) - GPIO5 (Pin 16)
-    * (12) D5(Bit 5) - GPIO6 (Pin 11)
-    * (13) D6(Bit 6) - GPIO13 (Pin 12)
-    * (14) D7(Bit 7) - GPIO19 (Pin 15)
+    * (11) D4(Bit 4) - GPIO5 (Pin 29)
+    * (12) D5(Bit 5) - GPIO6 (Pin 31)
+    * (13) D6(Bit 6) - GPIO13 (Pin 33)
+    * (14) D7(Bit 7) - GPIO19 (Pin 35)
     * (15) A(Backlight LED Anode. +) - 5V
     * (16) K(Backlight LED Cathode. -) - Ground
  */
