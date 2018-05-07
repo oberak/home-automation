6da82d4d600196fef156aa2c7448161433fffdb9
myathiriwinhtut <myatthiriwinhtut@gmail.com>
2018-05-07 18:30:48 +0630

    stream fix

--- stream/server.js
+++ stream/server.js
@@ -13,120 +13,85 @@ var serverIo = require('socket.io')(server);
 
 var Gpio = require('onoff').Gpio;
 var LED = new Gpio(2,'out');
-var lampbtn = new Gpio(3,'in', 'falling');
 var fan = new Gpio(17,'out');
-var fanbtn = new Gpio(27,'in','falling');
+var dec = new Gpio(27,'out');
+var btn = new Gpio(21, 'in','falling');
 
-lampbtn.watch(function(err, value) {
-  if (err) {
-    console.error('There was an error', err);
-  return;
-  }
-  LED.writeSync(LED.readSync() ^ 1);
+btn.watch(function(err, value) {
+ console.log('call bell');
+ if (err) {
+   console.error('There was an error', err);
+ return;
+ }
+ var player =Omx('./public/mp3/bell.wav');
+ setTimeout(
+     function() {
+     player.quit();
+ }, 3000);
 });
 
-fanbtn.watch(function(err, value) {
-  if (err) {
-    console.error('There was an error', err);
-  return;
-  }
-  console.log('fan button click');
-  fan.writeSync(fan.readSync() ^ 1);
-});
 function unexportOnClose() {
-  LED.writeSync(0);
-  LED.unexport();
-  lampbtn.unexport();
-  fan.writeSync(0);
-  fan.unexport();
-  fanbtn.unexport();
-};
+ LED.writeSync(0);
+ LED.unexport();
+ fan.writeSync(0);
+ fan.unexport();
+}
 process.on('SIGINT', unexportOnClose);
 
 var rfid = new Rfid(readRfid);
-var Gpio = require('pigpio').Gpio,
-    button = new Gpio(4, {
-        mode: Gpio.INPUT,
-        pullUpDown: Gpio.PUD_DOWN,
-        edge: Gpio.FALLING_EDGE
-    });
+// var Gpio = require('pigpio').Gpio,
+//     button = new Gpio(4, {
+//         mode: Gpio.INPUT,
+//         pullUpDown: Gpio.PUD_DOWN,
+//         edge: Gpio.FALLING_EDGE
+//     });
+//
+// button.on('interrupt', function (level) {
+//     console.log("level btn", level);
+//     // socket.emit('control', {
+//     //     type: DOORBTN,
+//     //     no: 0,
+//     //     falg: level
+//     var player =Omx('./public/mp3/bell.wav');
+//     setTimeout(
+//         function() {
+//         player.quit();
+//     }, (data.time) ? data.time * 1000 : 5000);
+//
+//     });
 
-button.on('interrupt', function (level) {
-    console.log("level btn", level);
-    // socket.emit('control', {
-    //     type: DOORBTN,
-    //     no: 0,
-    //     falg: level
-    var player =Omx('./public/mp3/bell.wav');
-    setTimeout(
-        function() {
-        player.quit();
-    }, (data.time) ? data.time * 1000 : 5000);
-
-    });
-}
 function readRfid(type, index, value) {
-    // send to socket
-    console.log(type, index, value);
-    socket.emit('control', {
-        type: type,
-        no: index,
-        falg: value
-    });
+   // send to socket
+   console.log(type, index, value);
+   socket.emit('control', {
+       type: type,
+       no: index,
+       falg: value
+   });
 }
 serverIo.on('connection', function(socket) {
-    socket.on('alarm', function(data) {
-        if (data.alarm) {
-            var player =Omx('./public/mp3/'+data.type+'.wav');
-            setTimeout(
-                function() {
-                player.quit();
-            }, (data.time) ? data.time * 1000 : 5000);
-        }
+   socket.on('alarm', function(data) {
+       if (data.alarm) {
+           var player =Omx('./public/mp3/'+data.type+'.wav');
+           setTimeout(
+               function() {
+               player.quit();
+           }, (data.time) ? data.time * 1000 : 5000);
+       }
 
-    });
+   });
+   socket.on('access', function (data) {
+     if (data.index == 0) {
+       LED.writeSync(LED.readSync() ^ 1);
+       return;
+       // console.log('call LED');
+     }else if (data.index == 1) {
+       fan.writeSync(fan.readSync() ^ 1);
+     }else if(data.index == 2) {
+       dec.writeSync(dec.readSync() ^ 1);
+     // console.log('call dec');
+   }else {
+     console.log('nothing');
+   }
+   });
 });
-// livecam
-var webcam_server = new LiveCam({
-    // address and port of the webcam UI
-    'ui_addr': config.stream.ip,
-    'ui_port': config.stream.port,
-
-    // address and port of the webcam Socket.IO server
-    // this server broadcasts GStreamer's video frames
-    // for consumption in browser side.
-    'broadcast_addr': config.stream.ip,
-    // 'broadcast_port' : 12000,
-
-    // address and port of GStreamer's tcp sink
-    // 'gst_tcp_addr' : 127.0.0.1,
-    // 'gst_tcp_port' : 10000,
-
-    // callback function called when server starts
-    'start': function() {
-        console.log('WebCam server started!');
-    },
-
-    // webcam object holds configuration of webcam frames
-    'webcam': {
-        // should frames be converted to grayscale (default : false)
-        //'grayscale' : true,
-
-        // should width of the frame be resized (default : 0)
-        // provide 0 to match webcam input
-        'width': 0, // 640, 1024,
-
-        // should height of the frame be resized (default : 0)
-        // provide 0 to match webcam input
-        'height': 0, //360,//768,
-
-        // should a fake source be used instead of an actual webcam
-        // suitable for debugging and development (default : false)
-        'fake': false,
-
-        // framerate of the feed (default : 0)
-        // provide 0 to match webcam input
-        'framerate': 25
-    }
-});
-webcam_server.broadcast();
